49e5a5f9fd237030a8b9440806e13dea83cd41a7
Anya <89126859788@mail.ru>
2024-11-27 11:43:35 +0500

    “Комментарий”

--- tema_10.py
+++ tema_10.py
@@ -1,12 +1,32 @@
-class Ivan:
-    __slots__ = ['name']
-    def __init__(self, name):
-        if name == 'Иван':
-            self.name = f"Да, я {name}"
-        else:
-            self.name = f"Я не {name}, а Иван"
-person1 = Ivan('Алексей')
-person2 = Ivan('Иван')
-print(person1.name)
-print(person2.name)
-person2.surname = 'Петров'
+import math
+
+class NegativeNumberError(Exception):
+    pass
+
+def calculate_square_root(num):
+    if num < 0:
+        raise NegativeNumberError("Ошибка: нельзя вычислить квадратный корень из отрицательного числа.")
+    return math.sqrt(num)
+
+def calculate_factorial(num):
+    if num < 0:
+        raise NegativeNumberError("Ошибка: факториал нельзя вычислить для отрицательного числа.")
+    if num == 0:
+        return 1
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+    return result
+
+
+try:
+    print(f'Квадратный корень из 16: {calculate_square_root(16)}')
+    print(f'Факториал числа 5: {calculate_factorial(5)}')
+    print(f'Квадратный корень из -4: {calculate_square_root(-4)}')
+except NegativeNumberError as e:
+    print(e)
+
+try:
+    print(f'Факториал числа -3: {calculate_factorial(-3)}')
+except NegativeNumberError as e:
+    print(e)
